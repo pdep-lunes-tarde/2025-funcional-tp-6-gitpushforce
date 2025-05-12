7e7e6e561b424d7eec5efe88b4ddce94dfa05b64
Juanma <juancitobravoc@gmail.com>
2025-05-12 00:46:09 -0300

    Primer implementacion

--- src/Library.hs
+++ src/Library.hs
@@ -2,19 +2,89 @@ module Library where
 import PdePreludat
 
 data Ingrediente =
-    Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras
+    Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras | Papas | PatiVegano | PanIntegral | BaconDeTofu
     deriving (Eq, Show)
 
+precioIngrediente :: Ingrediente -> Number
 precioIngrediente Carne = 20
 precioIngrediente Pan = 2
+precioIngrediente PanIntegral = 2
 precioIngrediente Panceta = 10
 precioIngrediente Cheddar = 10
 precioIngrediente Pollo =  10
 precioIngrediente Curry = 5
 precioIngrediente QuesoDeAlmendras = 15
+precioIngrediente Papas = 10
+precioIngrediente PatiVegano = 10
+precioIngrediente BaconDeTofu = 0
 
 data Hamburguesa = Hamburguesa {
     precioBase :: Number,
     ingredientes :: [Ingrediente]
 } deriving (Eq, Show)
 
+
+cuartoDeLibra :: Hamburguesa
+cuartoDeLibra = Hamburguesa {
+    precioBase = 20,
+    ingredientes = [Pan, Carne, Cheddar, Pan]
+}
+
+agregarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa
+agregarIngrediente ingrediente hamburguesa = hamburguesa {
+    ingredientes = ingrediente : ingredientes hamburguesa
+}
+
+ingredienteBase :: Hamburguesa -> Ingrediente
+ingredienteBase hamburguesa
+    | Carne `elem` ingredientes hamburguesa = Carne
+    | PatiVegano `elem` ingredientes hamburguesa = PatiVegano
+    | otherwise = Pollo
+
+agrandar :: Hamburguesa -> Hamburguesa
+agrandar hamburguesa = agregarIngrediente (ingredienteBase hamburguesa) hamburguesa
+
+descuento :: Number -> Hamburguesa -> Hamburguesa
+descuento unDescuento hamburguesa = hamburguesa {
+    precioBase = precioBase hamburguesa * (1 - (unDescuento / 100))
+}
+
+calcularPrecio :: Hamburguesa -> Number
+calcularPrecio hamburguesa = precioBase hamburguesa + sum (map precioIngrediente (ingredientes hamburguesa))
+
+pdepBurger :: Hamburguesa
+pdepBurger =
+    descuento 20 . agregarIngrediente Panceta . agregarIngrediente Cheddar . agrandar . agrandar $ cuartoDeLibra
+
+dobleCuarto :: Hamburguesa
+dobleCuarto = agregarIngrediente Carne . agregarIngrediente Cheddar $ cuartoDeLibra
+
+bigPdep :: Hamburguesa
+bigPdep = agregarIngrediente Curry dobleCuarto
+
+delDia :: Hamburguesa -> Hamburguesa
+delDia hamburguesa = descuento 30 . agregarIngrediente Papas $ hamburguesa
+
+cambiarPorVeggie :: Ingrediente -> Ingrediente
+cambiarPorVeggie Carne = PatiVegano
+cambiarPorVeggie Pollo = PatiVegano
+cambiarPorVeggie Cheddar = QuesoDeAlmendras
+cambiarPorVeggie Panceta = BaconDeTofu
+cambiarPorVeggie ingrediente = ingrediente
+
+hacerVeggie :: Hamburguesa -> Hamburguesa
+hacerVeggie hamburguesa = hamburguesa {
+    ingredientes = map cambiarPorVeggie (ingredientes hamburguesa)
+}
+
+cambiarPorPanIntegral :: Ingrediente -> Ingrediente
+cambiarPorPanIntegral Pan = PanIntegral
+cambiarPorPanIntegral ingrediente = ingrediente
+
+cambiarPanDePati :: Hamburguesa -> Hamburguesa
+cambiarPanDePati hamburguesa = hamburguesa {
+    ingredientes = map cambiarPorPanIntegral (ingredientes hamburguesa)
+}
+
+dobleCuartoVegano :: Hamburguesa
+dobleCuartoVegano = hacerVeggie . cambiarPanDePati $ dobleCuarto
--- src/Spec.hs
+++ src/Spec.hs
@@ -9,3 +9,68 @@ correrTests = hspec $ do
     describe "TP 5" $ do
         it "test de prueba" $ do
             2 + 2 `shouldBe` 4
+
+    describe "Parte 1" $ do
+        it "agrandar añade el ingrediente base al principio de la lista" $ do
+            ingredientes (agrandar cuartoDeLibra) `shouldBe` [Carne, Pan, Carne, Cheddar, Pan]
+  
+        it "agrandar añade Carne si ya hay Carne en la hamburguesa" $ do
+            ingredienteBase cuartoDeLibra `shouldBe` Carne
+            head (ingredientes (agrandar cuartoDeLibra)) `shouldBe` Carne
+  
+        it "agrandar añade Pollo si hay Pollo y no hay Carne" $ do
+            ingredienteBase (Hamburguesa 15 [Pan, Pollo, Cheddar, Pan]) `shouldBe` Pollo
+            head (ingredientes (agrandar (Hamburguesa 15 [Pan, Pollo, Cheddar, Pan]))) `shouldBe` Pollo
+  
+        it "agrandar añade PatiVegano si hay PatiVegano y no hay Carne ni Pollo" $ do
+            ingredienteBase (Hamburguesa 15 [Pan, PatiVegano, QuesoDeAlmendras, Pan]) `shouldBe` PatiVegano
+            head (ingredientes (agrandar (Hamburguesa 15 [Pan, PatiVegano, QuesoDeAlmendras, Pan]))) `shouldBe` PatiVegano
+  
+        it "agrandar prioriza Carne si hay tanto Carne como Pollo" $ do
+            ingredienteBase (Hamburguesa 15 [Pan, Carne, Pollo, Pan]) `shouldBe` Carne
+            head (ingredientes (agrandar (Hamburguesa 15 [Pan, Carne, Pollo, Pan]))) `shouldBe` Carne
+  
+        it "agrandar dos veces añade el ingrediente base dos veces" $ do
+            ingredientes (agrandar (agrandar cuartoDeLibra)) `shouldBe` [Carne, Carne, Pan, Carne, Cheddar, Pan]
+
+        it "agregarIngrediente añade un ingrediente a la hamburguesa" $ do
+            ingredientes (agregarIngrediente Panceta cuartoDeLibra) `shouldBe` [Panceta, Pan, Carne, Cheddar, Pan]
+  
+        it "descuento aplica el porcentaje correcto al precio base" $ do
+            precioBase (descuento 20 cuartoDeLibra) `shouldBe` 16
+  
+        it "pdepBurger tiene la composición y precio correcto" $ do
+            calcularPrecio pdepBurger `shouldBe` 110
+
+    describe "Parte 2" $ do
+        it "dobleCuarto tiene el precio correcto" $ do
+            calcularPrecio dobleCuarto `shouldBe` 84
+  
+        it "bigPdep tiene el precio correcto" $ do
+            calcularPrecio bigPdep `shouldBe` 89
+  
+        it "delDia aplica correctamente descuento y añade papas" $ do
+            head (ingredientes (delDia dobleCuarto)) `shouldBe` Papas
+            precioBase (delDia dobleCuarto) `shouldBe` precioBase dobleCuarto * 0.7
+            calcularPrecio (delDia dobleCuarto) `shouldBe` 88
+
+    describe "Parte 3" $ do
+        it "hacerVeggie reemplaza ingredientes por sus versiones veganas" $ do
+            Carne `notElem` ingredientes (hacerVeggie cuartoDeLibra) `shouldBe` True
+            Panceta `notElem` ingredientes (hacerVeggie (agregarIngrediente Panceta cuartoDeLibra)) `shouldBe` True
+            PatiVegano `elem` ingredientes (hacerVeggie cuartoDeLibra) `shouldBe` True
+            QuesoDeAlmendras `elem` ingredientes (hacerVeggie cuartoDeLibra) `shouldBe` True
+  
+        it "agrandar una hamburguesa veggie le agrega un PatiVegano" $ do
+            head (ingredientes (agrandar (hacerVeggie cuartoDeLibra))) `shouldBe` PatiVegano
+  
+        it "cambiarPanDePati reemplaza Pan por PanIntegral" $ do
+            Pan `notElem` ingredientes (cambiarPanDePati cuartoDeLibra) `shouldBe` True
+            PanIntegral `elem` ingredientes (cambiarPanDePati cuartoDeLibra) `shouldBe` True
+  
+        it "dobleCuartoVegano es un dobleCuarto veggie con pan integral" $ do
+            PatiVegano `elem` ingredientes dobleCuartoVegano `shouldBe` True
+            QuesoDeAlmendras `elem` ingredientes dobleCuartoVegano `shouldBe` True
+            PanIntegral `elem` ingredientes dobleCuartoVegano `shouldBe` True
+            Carne `notElem` ingredientes dobleCuartoVegano `shouldBe` True
+            Pan `notElem` ingredientes dobleCuartoVegano `shouldBe` True
